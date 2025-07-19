54682dfde131b44b7e708398dc259cd2037095ef
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-07-19 06:10:46 +0300

    Update Localization.lua

--- Localization.lua
+++ Localization.lua
@@ -19,11 +19,11 @@ return end
 if LOCALE == "deDE" then
 	-- German translations go here
 	L[" When:"] = " Wann:";			
-	L[" Where:"] = " Wo:";		-- SPLASH_BOOST_FEATURE1_TITLE
-	L[" "] = " Uhr";			-- TIMEMANAGER_TITLE
+	L[" Where:"] = " Wo:";		
+	L[" "] = " Uhr";			
 	L[" What:"] = " Was:";
-	L["Achievement"] = "Erfolg"			-- TRANSMOG_SOURCE_5
-	L["Transmogrification"] = "Transmogrifikation"		--SPLASH_LEGION_NEW_7_2_FEATURE2_TITLE
+	L["Achievement"] = "Erfolg"			
+	L["Transmogrification"] = "Transmogrifikation"		
 	L["<Right Click> for preview"] = "<Rechtsklick> für Vorschau"
 	L["<Left Click> for collection"] = "<Linksklick> für Sammlung"
 	L[" show me everything"] = " zeige allen Krempel"
@@ -51,63 +51,61 @@ if LOCALE == "deDE" then
 	L["Toys"]="Spielzeuge"
 return end
 
-
 if LOCALE == "ruRU" then
-	-- russian translations ZamestoTV
-L[" When:"] = "Когда:";			
-L[" Where:"] = "Где:";
-L[" "] = "";
-L[" What:"] = "Что:";
-L["Achievement"] = "Достижение"	
-L["Transmogrification"] = "Трансмогрификация"
-L["<Right Click> for preview"] = "<ПКМ> для предпросмотра"
-L["<Left Click> for collection"] = "<ЛКМ> для коллекции"
-L[" show me everything"] = " показать всё"
-L["|cFFff4040<Left Click>|r to open log"] = "|cFFff4040ЛКМ|r, чтобы открыть журнал"
-L["|cFFff4040<Right Click>|r to open options"] = "|cFFff4040ПКМ|r для настроек"
-L["log party loot"] = "записывать добычу группы"
-L["also logs loot of other players when in party or raid"] = "также записывает добычу других игроков в группе или рейде"
-L["show minimap button"] = "показывать кнопку у миникарты"
-L["show or hide that little icon on your minimap"] = "показывает или скрывает значок у миникарты"
-L[" also show party loot"] = " также показывать добычу группы"
-L["global save duration"] = "длительность хранения"
-L["number of days to keep all the informations"] = "количество дней для хранения информации"
-L["     Only days with records count towards this limit.\n     And obviously: More days need more memory!"] = "     Только дни с записями учитываются в этом лимите.\n     Очевидно: больше дней — больше памяти!"
-L["       Remark: This will take additional memory."] = "       Примечание: Это потребует дополнительной памяти."
-L[" › Junk"] = " › Хлам"
-L[" › Party Loot"] = " › Добыча группы"
-L["     Your selected filters also apply here.\n     Recent won't show up until you're out of combat!"] = "     Выбранные фильтры применяются и здесь.\n     Журнал не откроется, пока вы в бою!"
-L["show Recent if something is logged"] = "показывать журнал, если что-то записано"
-L["|cffADFF2F > wears iLvl "] = "|cffADFF2F > носит iLvl "
-L["|cFF999999 > wears iLvl "] = "|cFF999999 > носит iLvl "
-L["|cFF888888 > ... determining iLvl|r"] = "|cFF888888 > ... определение iLvl|r"
-L["Account-wide Settings"] = "Настройки для всего аккаунта"
-L["     Settings and filters are shared across all chars of this account."] = "Настройки и фильтры применяются ко всем персонажам аккаунта."
-L["|cFFFF5555Recent:|r Pet is still to learn!"] = "|cFFFF5555Журнал:|r Питомца ещё нужно изучить!"
-L["Toys"] = "Игрушки"
+	-- Russian translations ZamestoTV
+	L[" When:"] = " Когда:";  
+	L[" Where:"] = " Где:";
+	L[" "] = " ч";
+	L[" What:"] = " Что:";
+	L["Achievement"] = "Достижение";
+	L["Transmogrification"] = "Трансмогрификация";
+	L["<Right Click> for preview"] = "<ПКМ> для предварительного просмотра";
+	L["<Left Click> for collection"] = "<ЛКМ> для коллекции";
+	L[" show me everything"] = " показать всё";
+	L["|cFFff4040<Left Click>|r to open log"] = "|cFFff4040ЛКМ|r для открытия журнала";
+	L["|cFFff4040<Right Click>|r to open options"] = "|cFFff4040ПКМ|r для открытия настроек";
+	L["log party loot"] = "записывать добычу группы";
+	L["also logs loot of other players when in party or raid"] = "также записывает добычу других игроков в группе или рейде";
+	L["show minimap button"] = "показать кнопку на миникарте";
+	L["show or hide that little icon on your minimap"] = "показать или скрыть значок на миникарте";
+	L[" also show party loot"] = " добыча группы";
+	L["global save duration"] = "Глобальный срок хранения";
+	L["number of days to keep all the informations"] = "количество дней для хранения всей информации";
+	L["     Only days with records count towards this limit.\n     And obviously: More days need more memory!"] = "     Учитываются только дни с записями.\n     Очевидно, что больше дней требует больше памяти!";
+	L["       Remark: This will take additional memory."] = "       Примечание: Это потребует дополнительной памяти.";
+	L[" › Junk"] = " › Хлам";
+	L[" › Party Loot"] = " › Добыча группы";
+	L["     Your selected filters also apply here.\n     Recent won't show up until you're out of combat!"] = "     Выбранные фильтры также применяются здесь.\n     Журнал не отобразится, пока вы не выйдете из боя!";
+	L["show Recent if something is logged"] = "показать журнал, если что-то записано";
+	L["|cffADFF2F > wears iLvl "] = "|cffADFF2F > носит iLvl ";
+	L["|cFF999999 > wears iLvl "] = "|cFF999999 > носит iLvl ";
+	L["|cFF888888 > ... determining iLvl|r"] = "|cFF888888 > ... определение iLvl|r";
+	L["Account-wide Settings"] = "Настройки для всего аккаунта";
+	L["     Settings and filters are shared across all chars of this account."] = "Настройки и фильтры применяются ко всем персонажам этого аккаунта.";
+	L["|cFFFF5555Recent:|r Pet is still to learn!"] = "|cFFFF5555Журнал:|r Питомец ещё не изучен!";
+	L["Toys"] = "Игрушки";
 return end
 
-
 if LOCALE == "esES" then
-	-- spanish translations go here
+	-- Spanish translations go here
 	L[" When:"] = " cuándo:";			
-	L[" Where:"] = " donde:";		-- SPLASH_BOOST_FEATURE1_TITLE
+	L[" Where:"] = " donde:";		
 	L[" What:"] = " qué:";
-	L["Achievement"] = "éxito"			-- TRANSMOG_SOURCE_5
-	L["Transmogrification"] = "transmogrificación"		--SPLASH_LEGION_NEW_7_2_FEATURE2_TITLE
+	L["Achievement"] = "éxito"			
+	L["Transmogrification"] = "transmogrificación"		
 	L["<Right Click> for preview"] = "<clic derecho> para vista previa"
 	L["<Left Click> for collection"] = "<clic izquierdo> para colección"
 	L["|cFFff4040Click|r to open log"] = "|cFFff4040clic|r para recent"	
 return end
 
 if LOCALE == "frFR" then
-	-- french translations go here
+	-- French translations go here
 	L[" When:"] = " quand:";			
-	L[" Where:"] = " où:";		-- SPLASH_BOOST_FEATURE1_TITLE
-	L[" "] = " h";			-- TIMEMANAGER_TITLE
+	L[" Where:"] = " où:";		
+	L[" "] = " h";			
 	L[" What:"] = " ce qui:";
-	L["Achievement"] = "succès"			-- TRANSMOG_SOURCE_5
-	L["Transmogrification"] = "transmogrification"		--SPLASH_LEGION_NEW_7_2_FEATURE2_TITLE
+	L["Achievement"] = "succès"			
+	L["Transmogrification"] = "transmogrification"		
 	L["<Right Click> for preview"] = "<clic droit> pour aperçu"
 	L["<Left Click> for collection"] = "<clic gauche> pour collection"
 	L["|cFFff4040Click|r to open log"] = "|cFFff4040clic|r pour recent"
